513fc050fff1d488763be8e837208978d96545e4
xiwangly2 <1334850101@qq.com>
2023-10-25 17:48:14 +0800

    Sky: Children of the Light

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,6 +5,37 @@ export default defineUserConfig({
   title: 'Xiwangly\'s Page',
   description: '这是我的一个静态网站',
   theme: defaultTheme({
+    navbar: [
+      // 嵌套 Group - 最大深度为 2
+      {
+        text: 'Group',
+        children: [
+          {
+            text: 'SubGroup',
+            children: ['/group/sub/foo.md', '/group/sub/bar.md'],
+          },
+        ],
+      },
+      // 控制元素何时被激活
+      {
+        text: 'Group 2',
+        children: [
+          {
+            text: 'Always active',
+            link: '/',
+            // 该元素将一直处于激活状态
+            activeMatch: '/',
+          },
+          {
+            text: 'Active on /foo/',
+            link: '/not-foo/',
+            // 该元素在当前路由路径是 /foo/ 开头时激活
+            // 支持正则表达式
+            activeMatch: '^/foo/',
+          },
+        ],
+      },
+    ],
     locales: {
       '/en-US/': {
         selectLanguageName: 'English',
